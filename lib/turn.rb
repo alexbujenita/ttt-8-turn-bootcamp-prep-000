0efe4c441ede43483de29ba11e4fcbe8de694df0
dekc89 <dekc89@gmail.com>
2018-11-21 20:05:11 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,13 @@
+def display_board board
+  puts board  
+end
+
+
+
+def position_taken?(board, index)
+  if (board[index] == "X" || board[index] == "O")
+    return true
+  elsif (board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  end
+end
